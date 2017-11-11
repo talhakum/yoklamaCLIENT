418ceda866a12e8688f41578b0438b3947a0df74
Talha Kum <talha.kum@inomera.com>
2017-11-11 15:18:08 +0300

    Add Project

---
+++ Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace CoffeeMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.baglanButton = new System.Windows.Forms.Button();
+            this.koparButton = new System.Windows.Forms.Button();
+            this.portCombo = new System.Windows.Forms.ComboBox();
+            this.baudCombo = new System.Windows.Forms.ComboBox();
+            this.durumLabel = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.timerMAIN = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dersCombo = new System.Windows.Forms.ComboBox();
+            this.ogretimGorevlisiCombo = new System.Windows.Forms.ComboBox();
+            this.bolumCombo = new System.Windows.Forms.ComboBox();
+            this.errorProviderPort = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderRate = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderBolum = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderHoca = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderDers = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPort)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderRate)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderBolum)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderHoca)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderDers)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // baglanButton
+            // 
+            this.baglanButton.Location = new System.Drawing.Point(70, 224);
+            this.baglanButton.Name = "baglanButton";
+            this.baglanButton.Size = new System.Drawing.Size(66, 59);
+            this.baglanButton.TabIndex = 0;
+            this.baglanButton.Text = "Bağlan";
+            this.baglanButton.UseVisualStyleBackColor = true;
+            this.baglanButton.Click += new System.EventHandler(this.baglanButton_Click);
+            // 
+            // koparButton
+            // 
+            this.koparButton.Enabled = false;
+            this.koparButton.Location = new System.Drawing.Point(265, 224);
+            this.koparButton.Name = "koparButton";
+            this.koparButton.Size = new System.Drawing.Size(66, 59);
+            this.koparButton.TabIndex = 1;
+            this.koparButton.Text = "Kopar";
+            this.koparButton.UseVisualStyleBackColor = true;
+            this.koparButton.Click += new System.EventHandler(this.koparButton_Click);
+            // 
+            // portCombo
+            // 
+            this.portCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.portCombo.FormattingEnabled = true;
+            this.portCombo.Location = new System.Drawing.Point(223, 23);
+            this.portCombo.Name = "portCombo";
+            this.portCombo.Size = new System.Drawing.Size(108, 21);
+            this.portCombo.TabIndex = 2;
+            // 
+            // baudCombo
+            // 
+            this.baudCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudCombo.FormattingEnabled = true;
+            this.baudCombo.Location = new System.Drawing.Point(223, 66);
+            this.baudCombo.Name = "baudCombo";
+            this.baudCombo.Size = new System.Drawing.Size(108, 21);
+            this.baudCombo.TabIndex = 3;
+            this.baudCombo.SelectedIndexChanged += new System.EventHandler(this.baudCombo_SelectedIndexChanged);
+            // 
+            // durumLabel
+            // 
+            this.durumLabel.AutoSize = true;
+            this.durumLabel.Location = new System.Drawing.Point(183, 247);
+            this.durumLabel.Name = "durumLabel";
+            this.durumLabel.Size = new System.Drawing.Size(36, 13);
+            this.durumLabel.TabIndex = 4;
+            this.durumLabel.Text = "Kapalı";
+            // 
+            // timerMAIN
+            // 
+            this.timerMAIN.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(67, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Portlar";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(67, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Bağlantı Hızı";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(67, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Bölüm";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(67, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Öğretim Görevlisi";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(67, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(29, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Ders";
+            // 
+            // dersCombo
+            // 
+            this.dersCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.dersCombo.Location = new System.Drawing.Point(223, 183);
+            this.dersCombo.Name = "dersCombo";
+            this.dersCombo.Size = new System.Drawing.Size(108, 21);
+            this.dersCombo.TabIndex = 2;
+            this.dersCombo.SelectedIndexChanged += new System.EventHandler(this.dersCombo_SelectedIndexChanged);
+            // 
+            // ogretimGorevlisiCombo
+            // 
+            this.ogretimGorevlisiCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.ogretimGorevlisiCombo.Location = new System.Drawing.Point(223, 142);
+            this.ogretimGorevlisiCombo.Name = "ogretimGorevlisiCombo";
+            this.ogretimGorevlisiCombo.Size = new System.Drawing.Size(108, 21);
+            this.ogretimGorevlisiCombo.TabIndex = 1;
+            this.ogretimGorevlisiCombo.SelectedIndexChanged += new System.EventHandler(this.ogretimGorevlisiCombo_SelectedIndexChanged);
+            // 
+            // bolumCombo
+            // 
+            this.bolumCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.bolumCombo.Items.AddRange(new object[] {
+            "1",
+            "2"});
+            this.bolumCombo.Location = new System.Drawing.Point(223, 108);
+            this.bolumCombo.Name = "bolumCombo";
+            this.bolumCombo.Size = new System.Drawing.Size(108, 21);
+            this.bolumCombo.TabIndex = 0;
+            this.bolumCombo.SelectedIndexChanged += new System.EventHandler(this.bolumCombo_SelectedIndexChanged);
+            // 
+            // errorProviderPort
+            // 
+            this.errorProviderPort.ContainerControl = this;
+            // 
+            // errorProviderRate
+            // 
+            this.errorProviderRate.ContainerControl = this;
+            // 
+            // errorProviderBolum
+            // 
+            this.errorProviderBolum.ContainerControl = this;
+            // 
+            // errorProviderHoca
+            // 
+            this.errorProviderHoca.ContainerControl = this;
+            // 
+            // errorProviderDers
+            // 
+            this.errorProviderDers.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(385, 318);
+            this.Controls.Add(this.bolumCombo);
+            this.Controls.Add(this.ogretimGorevlisiCombo);
+            this.Controls.Add(this.dersCombo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.durumLabel);
+            this.Controls.Add(this.baudCombo);
+            this.Controls.Add(this.portCombo);
+            this.Controls.Add(this.koparButton);
+            this.Controls.Add(this.baglanButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPort)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderRate)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderBolum)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderHoca)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderDers)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button baglanButton;
+        private System.Windows.Forms.Button koparButton;
+        private System.Windows.Forms.ComboBox portCombo;
+        private System.Windows.Forms.ComboBox baudCombo;
+        private System.Windows.Forms.Label durumLabel;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Timer timerMAIN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox dersCombo;
+        private System.Windows.Forms.ComboBox ogretimGorevlisiCombo;
+        private System.Windows.Forms.ComboBox bolumCombo;
+        private System.Windows.Forms.ErrorProvider errorProviderPort;
+        private System.Windows.Forms.ErrorProvider errorProviderRate;
+        private System.Windows.Forms.ErrorProvider errorProviderBolum;
+        private System.Windows.Forms.ErrorProvider errorProviderHoca;
+        private System.Windows.Forms.ErrorProvider errorProviderDers;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.IO;
+using System.Text.RegularExpressions;
+using MySql.Data.MySqlClient;
+using System.Net;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using System.Dynamic;
+using System.Net.Http;
+using System.Globalization;
+
+namespace CoffeeMachine
+{
+    public partial class Form1 : Form
+    {
+        string[] ports = SerialPort.GetPortNames();
+        private static readonly HttpClient client = new HttpClient();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (string port in ports)
+            {
+                portCombo.Items.Add(port); // Port isimlerini combobox1'de gösteriyoruz.
+                portCombo.SelectedIndex = 0;
+            }
+            baudCombo.Items.Add("2400");  // Baudrate'leri kendimiz combobox2'ye giriyoruz.
+            baudCombo.Items.Add("4800");
+            baudCombo.Items.Add("9600");
+            baudCombo.Items.Add("19200");
+            baudCombo.Items.Add("115200");
+            baudCombo.SelectedIndex = 2;
+
+            // Fill bolum combobox
+            string htmlBolum = string.Empty;
+            string urlBolum = @"http://localhost:8080/eyoklama/bolum";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(urlBolum);
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                htmlBolum = reader.ReadToEnd();
+            }
+
+            dynamic dynBolum = JsonConvert.DeserializeObject<JArray>(htmlBolum);
+
+            for (int i = 0; i < dynBolum.Count; i++) {
+                bolumCombo.Items.Add(dynBolum[i].isim);
+            }
+
+            
+        }
+            
+       
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+            }
+        }
+      
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                string sonuc = serialPort1.ReadExisting();//Serial.print kodu ile gelen analog veriyi alıyoruz,string formatında sonuc'a atıyoruz
+
+                Regex regkhv1 = new Regex("Card UID: [0-F][0-F] [0-F][0-F] [0-F][0-F] [0-F][0-F]");
+                string CardUID=regkhv1.Match(sonuc).Value;
+                if(CardUID!=null && CardUID.Length>20) {
+					string uid = String.Concat(CardUID[10], CardUID[11], CardUID[13], CardUID[14], CardUID[16], CardUID[17], CardUID[19], CardUID[20]);
+                    ogrenciKayit(uid);
+                    Console.WriteLine(uid);
+                }
+            }
+            catch (Exception ex)
+            {
+                timerMAIN.Stop();
+                MessageBox.Show("Zamanlayıcı hatası : "+ex.Message);
+                
+                
+            }
+        }
+
+        private void ogrenciKayit(string uid) {
+            
+            // Add roll call
+
+            var httpWebRequest = (HttpWebRequest)WebRequest.Create("http://localhost:8080/eyoklama/yoklama");
+            httpWebRequest.ContentType = "application/json";
+            httpWebRequest.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+            {
+                string json = "{\"tarih\":\""+DateTime.Now.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture)+"\"," +
+                              "\"rfid\":\""+uid+"\"," +
+                                                      "\"ders_name\":\""+dersCombo.SelectedItem.ToString()+"\"," +
+                                                   "\"saat\":\""+ DateTime.Now.ToString("hh:mm:ss")+"\"}";
+
+                streamWriter.Write(json);
+                streamWriter.Flush();
+                streamWriter.Close();
+            }
+
+            var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+            using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+            {
+                var result = streamReader.ReadToEnd();
+            }
+
+        }
+
+        private bool isFormFilled()
+        {
+            bool flag=true;
+            if (portCombo.SelectedIndex > -1)
+            {
+                errorProviderPort.Clear();
+            }
+            else
+            {
+                errorProviderPort.SetError(portCombo, "Lütfen bir port seçiniz");
+                flag = false;
+            }
+            if (baudCombo.SelectedIndex > -1)
+            {
+                errorProviderRate.Clear();
+            }
+            else
+            {
+                errorProviderRate.SetError(baudCombo, "Lütfen bağlantı hızını seçiniz");
+                flag = false;
+            }
+            if (bolumCombo.SelectedIndex > -1)
+            {
+                errorProviderBolum.Clear();
+            }
+            else
+            {
+                errorProviderBolum.SetError(bolumCombo, "Lütfen bir bölüm seçiniz");
+                flag = false;
+            }
+            if (ogretimGorevlisiCombo.SelectedIndex > -1)
+            {
+                errorProviderHoca.Clear();
+            }
+            else
+            {
+                errorProviderHoca.SetError(ogretimGorevlisiCombo, "Lütfen öğretim görevlisi alanını doldurunuz");
+                flag = false;
+            }
+            if (dersCombo.SelectedIndex > -1)
+            {
+                errorProviderDers.Clear();
+            }
+            else
+            {
+                errorProviderDers.SetError(dersCombo, "Lütfen bir ders seçiniz");
+                flag = false;
+            }
+
+
+
+            return flag;
+        }
+
+        private void baglanButton_Click(object sender, EventArgs e)
+        {
+            // Check if form is filled
+
+            if (!isFormFilled())
+            {
+                return;
+            }
+            
+            timerMAIN.Start(); 
+            if (serialPort1.IsOpen == false)
+            {
+                if (portCombo.Text == "")
+                    return;
+
+                serialPort1.PortName = portCombo.Text; 
+                //serialPort1.PortName = "/dev/cu.wchusbserial1410";
+				serialPort1.BaudRate = Convert.ToInt16(baudCombo.Text); 
+                
+
+                try
+                {
+                    serialPort1.Open(); //Haberleşme için port açılıyor
+                    durumLabel.ForeColor = Color.Green;
+                    durumLabel.Text = "Bağlantı Açık";
+                    baglanButton.Enabled = false;
+                    koparButton.Enabled = true;
+                    baudCombo.Enabled = false;
+                    portCombo.Enabled = false;
+
+                }
+                catch (Exception hata)
+                {
+
+                    MessageBox.Show("Hata : " + hata.Message);
+                   
+                    
+                }
+            }
+            else
+            {
+                durumLabel.Text = "Bağlantı kurulu !!!";
+            }
+        }
+
+        private void koparButton_Click(object sender, EventArgs e)
+        {
+            timerMAIN.Stop();
+            if (serialPort1.IsOpen == true)
+            {
+                serialPort1.Close();
+                durumLabel.ForeColor = Color.Red;
+                durumLabel.Text = "Bağlantı Kapalı";
+                baglanButton.Enabled = true;
+                baudCombo.Enabled = true;
+                portCombo.Enabled = true;
+                koparButton.Enabled = false;
+            }
+        }
+
+      
+
+        private void baudCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void bolumCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Fill ogretimGorevlisi combobox
+            ogretimGorevlisiCombo.Items.Clear();
+            string htmlBolum = string.Empty;
+            string urlBolum = @"http://localhost:8080/eyoklama/ogretimgorevlisi/"+bolumCombo.SelectedItem+"";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Uri.EscapeUriString(urlBolum));
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                htmlBolum = reader.ReadToEnd();
+            }
+
+            dynamic dynBolum = JsonConvert.DeserializeObject<JArray>(htmlBolum);
+
+            for (int i = 0; i < dynBolum.Count; i++)
+            {
+                ogretimGorevlisiCombo.Items.Add(dynBolum[i][0].ad + " " + dynBolum[i][0].soyad);
+            }
+        }
+
+        private void ogretimGorevlisiCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Fill ders combobox
+            dersCombo.Items.Clear();
+
+            var selectedItem = ogretimGorevlisiCombo.SelectedItem.ToString();
+            var hocaSoyad = selectedItem.Split(' ').Last();
+
+            string htmlBolum = string.Empty;
+            string urlBolum = @"http://localhost:8080/eyoklama/ders/"+hocaSoyad+"";
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Uri.EscapeUriString(urlBolum));
+            request.AutomaticDecompression = DecompressionMethods.GZip;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                htmlBolum = reader.ReadToEnd();
+            }
+
+            dynamic dynBolum = JsonConvert.DeserializeObject<JArray>(htmlBolum);
+
+            for (int i = 0; i < dynBolum.Count; i++)
+            {
+                dersCombo.Items.Add(dynBolum[i][1].ad);
+            }
+
+            
+        }
+
+        private void dersCombo_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
